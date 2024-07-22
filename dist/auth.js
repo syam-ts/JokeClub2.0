977de847213019bfa507774a398709829a9a4432
| ｓｙａｍ | <syamnandhu3@gmail.com>
2024-07-22 22:00:04 +0530

    bugs

---
+++ dist/auth.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+var passport = require("passport");
+router.get("/login/success", (req, res) => {
+    if (req.user) {
+        res.render("home");
+    }
+    else {
+        res.status(403).json({ error: true, message: "Not Authorized" });
+    }
+});
+router.get("/login/failed", (req, res) => {
+    res.status(401).json({ error: true, message: "Log in failure" });
+});
+router.get("/google", passport.authenticate("google", { scope: ["email", "profile"] }));
+router.get("/google/callback", passport.authenticate("google", {
+    failureRedirect: "/login/failed",
+}), (req, res) => {
+    res.redirect("/home");
+});
+router.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect(process.env.CLIENT_URL);
+});
+module.exports = router;
