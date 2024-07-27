3b26e702d4c71f28200fb76a398e436459e92cc7
| ｓｙａｍ | <syamnandhu3@gmail.com>
2024-07-27 11:56:44 +0530

    seiralizer

--- auth.ts
+++ auth.ts
@@ -1,23 +1,24 @@
-const passport = require('passport');
+var passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
 passport.use(new GoogleStrategy({
-    clientID:     GOOGLE_CLIENT_ID,
+    clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
     callbackURL: "http://localhost:3000/google/callback",
-    passReqToCallback   : true
+    passReqToCallback : true
   },
-  function(request, accessToken, refreshToken, profile, done) {
+  function(request, accessToken, refreshToken,err,  profile, done) {
    return done(err, profile);
   }
 ));
 
 passport.serializeUser(function(user, done) {
-      done(nulll, user);
-})
+      done(null, user);
+});
+
 passport.deserializeUser(function(user, done) {
-      done(nulll, user);
-})
+      done(null, user);
+});
