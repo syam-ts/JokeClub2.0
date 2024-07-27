9052d506eb7794abca1711f96e900f4048c954bf
| ｓｙａｍ | <syamnandhu3@gmail.com>
2024-07-27 11:50:02 +0530

    google auth

--- routes/home.ts
+++ routes/home.ts
@@ -1,11 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+require('../auth');
 
 router.get('/home', (req, res) => {
     res.render('home');
 });
 router.get('/login', (req, res) => {
-    res.render('login');
+    res.send('<a href="/auth/google">Authenticate with Google</a>');
+});
+
+router.get('/auth/google' ,(req, res) => {
+    passport.authenicate('google', {scope : ['profile', 'email']});
+})
+ 
+
+router.get('/protected' , (req, res) => {
+    res.send('Successed');
 });
 
 
