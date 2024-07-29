ac27268efc774db1149c39190e5498a0cd57c2ee
| ｓｙａｍ | <syamnandhu3@gmail.com>
2024-07-29 23:15:37 +0530

    g

--- routes/home.ts
+++ routes/home.ts
@@ -59,4 +59,21 @@ router.get('/auth/google/failure', (req, res) => {
   });
 
 
+  var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth2').Strategy;
+passport.use(new GoogleStrategy({
+    clientID: 'GOOGLE_CLIENT_ID',
+    clientSecret: 'GOOGLE_CLIENT_SECRET',
+    callbackURL: "http://localhost:0/auth/google/callback",
+    passReqToCallback: true,
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+    request.session.regenerate(function(err) {
+      if (err) {
+        return done(err);
+      }
+      return done(null, profile);
+    });
+  }));
+
 module.exports = router;
